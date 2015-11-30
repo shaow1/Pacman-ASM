c4ba98918527b11212b2ffea2f682340dacd6a02
Mallory Harper <harperm@wit.edu>
2015-11-30 16:15:07 -0500

    correct spalsh screen update

--- pacman.asm
+++ pacman.asm
@@ -20,53 +20,10 @@ leftKey = 19200
 maxScore = 900; 21300
 
 .data
-<<<<<<< HEAD
-	boardgame BYTE BUFFER_SIZE DUP(?)
-	buffer BYTE BUFFER_SIZE DUP(?)
-	buffer1 BYTE BUFFER_SIZE DUP(?)
-	filename  BYTE "FinalBoardGame.txt",0
-	directionfile BYTE "DirectionFile.txt", 0
-	splashfile BYTE "asciipacmanart.txt",0
-	scoreString  BYTE "Score: ",0
-	levelString  BYTE "Level: ",0
-	wincaption  BYTE "WINNER!",0
-	winnermsg  BYTE "Congrats! You have beaten all 6 levels. You are Pac Man Camp.",0
-	fileHandle HANDLE ?
-
-	nextTick dd 0
-	lengthOfFrame dd 200
-	lastKeyPressed dw 19712
-
-	x byte 1
-	y byte 1
-	intendedX byte 2
-	intendedY byte 1
-
-	MessBoxTitle db "Level Complete",0
-	MessBox db "Level Complete: Do you wish to continute?",0 
-
-	MessBoxTitle1 db "Fail",0
-	MessBox1 db "You have fallen in a hole! Do you wish to restart?",0 
-
-	Welcome db "Welcome To the Best Pac Man Game Ever!", 0
-	CreatedBy db "Created By: Mallory Harper, Chris Chevalier, Greg Shao",0
-	NextStep db "Begin Playing (1) or Need Directions (2)? ", 0
-	directionContinue db "Now that you read the directions would you like to play? Yes (1) or No (2)?: ",0
-	invalidnumber db "Invalid Number, Please enter either 1 or 2", 0
-
-	score dd 0
-	level dd 1
-	yvalue db 20
-	xvalue db 25
-	inputnumber dd 0
-	directionumber dd 0
-	fuckyou dd 0
-=======
 	;gameboard
 		boardgame BYTE BUFFER_SIZE DUP(?)
 		buffer BYTE BUFFER_SIZE DUP(?)
 		filename  BYTE "FinalBoardGame.txt",0
-		directionfile BYTE "DirectionFile.txt", 0
 		fileHandle HANDLE ?
 	
 	;game strings
@@ -80,7 +37,6 @@ maxScore = 900; 21300
 
 			MessBoxTitle1 db "Fail",0
 			MessBox1 db "You have fallen in a hole! Do you wish to restart?", 0 
->>>>>>> 349a7428e07b86197650dceafc2a0db76d40635d
 	
 	;game variables
 		nextTick dd 0
@@ -95,6 +51,10 @@ maxScore = 900; 21300
 		intendedY byte 1
 
 	;splashScreen Variables
+		buffer1 BYTE BUFFER_SIZE DUP(?)
+		directionfile BYTE "DirectionFile.txt", 0
+		splashfile BYTE "asciipacmanart.txt",0
+
 		Welcome db "Welcome To the Best Pac Man Game Ever!", 0
 		CreatedBy db "Created By: Mallory Harper, Chris Chevalier, Greg Shao",0
 		NextStep db "Begin Playing (1) or Need Directions (2)? ", 0
@@ -139,135 +99,6 @@ gameLoop proc
 
 	;ret
 gameLoop endp
-<<<<<<< HEAD
-;-------------------------------------------------------------------------------------------
-;Splash Screen
-splashscreen PROC
-
-
-	mov edx, 0
-	mov dh, 15
-	call Gotoxy
-	mov edx, Offset splashfile
-	call openinputfile
-	mov filehandle, eax
-	mov edx, offset buffer1
-	mov ecx, buffer_size
-	call ReadFromFile
-	mov buffer1[eax],0
-	mov edx, offset buffer1
-	mov eax,lightblue
-	call SetTextColor
-	call writestring
-	call crlf
-	mov eax, filehandle
-	call closefile
-
-
-
-	mov edx, 0
-	mov dh, yvalue
-	mov dl, xvalue
-	call Gotoxy
-	mov edx, offset Welcome
-	mov eax,lightGreen
-	call SetTextColor
-	call WriteString
-
-	call crlf
-	inc yvalue
-	mov dh, yvalue
-	mov dl,15
-	call Gotoxy
-	mov edx, offset CreatedBy
-	mov eax,magenta
-	call SetTextColor
-	call WriteString
-
-	call crlf
-	inc yvalue
-	mov dh, yvalue
-	mov dl,23
-	call Gotoxy
-	mov edx, offset NextStep
-	mov eax,cyan
-	call SetTextColor
-	call WriteString
-	call Readint
-	mov inputnumber, eax
-
-	mov eax, 15 ;changes color back to normal
-	call SetTextColor
-
-	cmp inputnumber, 1
-	je beginplaying
-	cmp inputnumber, 2
-	je directions
-	jmp invalidnum
-
-	beginplaying:
-		call clrscr
-		call LoadGameBoardFile
-		call drawscreen
-		call gameLoop
-		call crlf
-		ret
-
-	directions:
-		call clrscr
-		call printdirections
-
-		mov edx, offset directionContinue
-		call WriteString
-		call Readint
-		mov directionumber, eax
-
-		cmp directionumber, 1
-		je beginplaying
-		cmp directionumber, 2
-		je quitgame
-		jmp invalidnum
-
-
-		quitgame:
-			exit
-		ret
-
-	invalidnum:
-		call clrscr
-		mov dh, 19
-		mov dl, 24
-		call Gotoxy
-		mov edx, offset invalidnumber
-		call WriteString
-		call splashscreen
-		call crlf
-
-		ret
-
-
-splashscreen ENDP
-
-printdirections PROC
-	
-	mov edx, Offset directionfile
-	call openinputfile
-	mov filehandle, eax
-	mov edx, offset buffer
-	mov ecx, buffer_size
-	call ReadFromFile
-	mov buffer[eax],0
-	mov edx, offset buffer
-	call writestring
-	call crlf
-	mov eax, filehandle
-	call closefile
-
-
-ret
-printdirections ENDP
-=======
->>>>>>> 349a7428e07b86197650dceafc2a0db76d40635d
 
 ;---------------------------------------------------------------------------
 ;User Input
@@ -411,41 +242,6 @@ moveCharacter proc
 		mov intendedY, 11
 		call movePacMan
 		ret
-<<<<<<< HEAD
-		
-moveCharacter endp
-
-endGame PROC
-	INVOKE MessageBox, NULL, ADDR Messbox1,
-	ADDR MessBoxTitle1, MB_YESNO + MB_ICONQUESTION
-	cmp eax,IDYES
-	call restartGame
-	;exit
-	ret
-endGame ENDP
-
-restartGame PROC
-	mov x, 1
-	mov y, 1
-	mov intendedX, 2
-	mov intendedY, 1
-	mov lastKeyPressed, 19712
-
-	mov score,0 ;puts score back at zero for the beginning level
-	mov level, 1
-
-	call clrscr
-	call LoadGameBoardFile
-	call drawscreen
-	;call updateScore
-
-	mov dl, x
-	mov dh, y
-	mov al, "@"
-	call writeToScreen	;draw pacman
-
-=======
->>>>>>> 349a7428e07b86197650dceafc2a0db76d40635d
 	
 	ret
 moveCharacter endp
@@ -640,54 +436,6 @@ updateScore proc
 	ret		
 updateScore endp
 
-<<<<<<< HEAD
-nextlevel Proc
-	cmp score, 21300
-	;cmp score, 600
-	je printmessage
-	jmp somewhere
-
-
-;cmp score, 21300
-;cmp score, 600
-;je printmessage
-;jmp somewhere
-
-	printmessage:
-		INVOKE MessageBox, NULL, ADDR Messbox,
-		ADDR MessBoxTitle, MB_YESNO + MB_ICONQUESTION
-		cmp eax,IDYES
-		je increaselevel
-		jmp somewhere
-
-
-	increaselevel:
-		mov score,0 ;puts score back at zero for the next level
-		inc level
-
-		mov x, 1
-		mov y, 1
-		mov intendedX, 2
-		mov intendedY, 1
-		mov lastKeyPressed, 19712
-
-		call clrscr
-		call LoadGameBoardFile
-		call drawscreen
-		;call updateScore
-
-		mov dl, x
-		mov dh, y
-		mov al, "@"
-		call writeToScreen	;draw pacman
-
-	somewhere:
-		ret
-
-nextlevel ENDP
-
-=======
->>>>>>> 349a7428e07b86197650dceafc2a0db76d40635d
 ;---------------------------------------------------------------------------
 ;Gameboard Array Procedures
 
@@ -728,7 +476,25 @@ writeToArray endp
 ;-------------------------------------------------------------------------------------------
 ;Splash Screen
 splashscreen PROC
-	call clrscr
+
+	mov edx, 0
+	mov dh, 15
+	call Gotoxy
+	mov edx, Offset splashfile
+	call openinputfile
+	mov filehandle, eax
+	mov edx, offset buffer1
+	mov ecx, buffer_size
+	call ReadFromFile
+	mov buffer1[eax],0
+	mov edx, offset buffer1
+	mov eax,lightblue
+	call SetTextColor
+	call writestring
+	call crlf
+	mov eax, filehandle
+	call closefile
+
 
 	mov edx, 0
 	mov dh, yvalue
@@ -809,6 +575,8 @@ splashscreen PROC
 		call crlf
 
 		ret
+
+
 splashscreen ENDP
 
 printdirections PROC
@@ -826,9 +594,9 @@ printdirections PROC
 	mov eax, filehandle
 	call closefile
 
-	ret
-printdirections ENDP
 
+ret
+printdirections ENDP
 ;---------------------------------------------------------------------------
 
 END main
